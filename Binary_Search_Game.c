8dbe126647690bd25c0235f01ede4ada0af6bd23
the-geth <storm.blair@yahoo.com>
2023-07-05 12:44:54 -0900

    working on random number program

---
+++ Binary_Search_Game.c
@@ -0,0 +1,18 @@
+/*this is a simple program to demonstrate how a binary search works.
+ * the computer will determine an upper bound and then pick a random number between it and 1. the computer will then determine the number of guesses to ensure the player always has enough to find the number, provided they know how to use a binary search*/
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+int main(){
+	//random number = (rand()%(upper-lower+1))+lower
+	srand(time(NULL));
+	int upper_bound = (rand()%(1000-1+1))+1;
+	int random_number = (rand()%(upper_bound-1+1))+1;
+	//log change of base formula
+	int num_guesses = (log(upper_bound)/log(2));
+	printf("welcome to a guessing game you can always win\n ");
+	printf("im thinking of a number between 1 and %d\n", upper_bound);
+	printf("you have %d guesses and I will tell you if you are high or low\n", num_guesses);
+	return 0;
+}
